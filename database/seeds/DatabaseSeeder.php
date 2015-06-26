ee5a96e9e61b6cabcbda02d60033a659c8fd16ad
saminanirmala <samina.tuladhar7@gmail.com>
2015-06-27 01:40:11 +0545

    dummy username and password given

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,20 +1,39 @@
 <?php
 
+
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Model::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call('UserTableSeeder');
 
-		// $this->call('UserTableSeeder');
-	}
+        $this->command->info('User table seeded!');
+
+    }
+}
+
+class UserTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('tbl_user')->delete();
+        $email = User::EMAIL;
+        $password = Hash::make(User::PASSWORD);
+
+        User::create(array('email' => $email ,
+                'password' => $password)
+        );
+    }
 
 }
